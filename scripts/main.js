9ec8b8eaacb842a1bd172f14d2364c1d84dfddf7
Luis Torres <luis.torres@tyzcorp.com>
2023-09-03 16:42:16 -0300

    Subida nuevo index

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const typed = new Typed(`.typed`, {
+  //   strings: [
+  //     `<i class="typedTexto">Gato</i>`,
+  //     `<i class="typedTexto">Perro</i>`,
+  //     `<i class="typedTexto">Conejo</i>`,
+  //     `<i class="typedTexto">Pez</i>`,
+  //   ],
+  stringsElement: `#cadenas-texto`,
+  typeSpeed: 75,
+  startDelay: 10,
+  backSpeed: 75,
+  smartBackspace: true,
+  shuffle: false,
+  backDelay: 1500,
+  loop: true,
+  showCursor: true,
+  cursoChar: `|`,
+  contentType: `html`,
+});
