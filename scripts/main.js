36c6eb08e6c17232e332125d0fff47ddd484ebcb
Luis Torres <luis.torres@tyzcorp.com>
2023-09-20 00:22:01 -0300

    Subida nuevo index

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,6 @@ import { returnSpecsWPC } from "./common_pages/specs.js";
 import { returnVentajas } from "./common_pages/ventajas.js";
 import { traerProducto } from "./json_connection.js";
 
-document.getElementById("specsWPC").innerHTML = returnSpecsWPC(0);
-document.getElementById("faqs").innerHTML = returnFaqs();
-document.getElementById("ventajas").innerHTML = returnVentajas(0);
-document.getElementById("selloCalidad").innerHTML = returnSello(0);
-
 // animación en hero
 const typed = new Typed(`.typed`, {
   stringsElement: `#cadenas-texto`,
@@ -23,6 +18,11 @@ const typed = new Typed(`.typed`, {
   cursoChar: `|`,
   contentType: `html`,
 });
+
+document.getElementById("specsWPC").innerHTML = returnSpecsWPC(0);
+document.getElementById("faqs").innerHTML = returnFaqs();
+document.getElementById("ventajas").innerHTML = returnVentajas(0);
+document.getElementById("selloCalidad").innerHTML = returnSello(0);
 
 document.getElementById("btnDiseno").addEventListener("click", () => {
   document.getElementById("nombreDiseno").value =
@@ -97,70 +97,67 @@ document
     }
   });
 
-PintarProductosTOP();
-async function PintarProductosTOP() {
-  const listaProductoIndex = await traerProducto();
+// const listaProductoIndex = await traerProducto();
 
-  // PINTAR PRODUCTOS
+// PINTAR PRODUCTOS
 
-  let mensajeProductos = `<div id="productos" class="row col-12 text-center">
+let mensajeProductos = `<div id="productos" class="row col-12 text-center">
   <h3>Productos TOP</h3>
   </div>
   <div class="row d-flex justify-content-around">`;
 
-  listaProductoIndex.forEach((element) => {
-    let mensajeColor = "";
+// listaProductoIndex.forEach((element) => {
+//   let mensajeColor = "";
 
-    mensajeProductos =
-      mensajeProductos +
-      `<div class="col-11 col-sm-11 col-md-4 col-lg-4 col-xl-4">
-        <div class="card">
-        <div class="card-product-image">
-        <img src="./images/productos/perfil/${element.html_name}.png" />
-        <div class="card-product-image-type text-end">
-        <span>${element.ambientes}</span>
-        </div>
-        </div>
-        <div class="card-body">
-        <h5 class="card-title">${element.nombre}</h5>
-        <p class="card-text">
-        ${element.descripcion_corta}
-        </p>
-        <div class="card-text d-flex flex-row">
-        <div><p>Colores:</p></div>
-        <div class="d-flex colors">`;
+//   mensajeProductos =
+//     mensajeProductos +
+//     `<div class="col-11 col-sm-11 col-md-4 col-lg-4 col-xl-4">
+//         <div class="card">
+//         <div class="card-product-image">
+//         <img src="./images/productos/perfil/${element.html_name}.png" />
+//         <div class="card-product-image-type text-end">
+//         <span>${element.ambientes}</span>
+//         </div>
+//         </div>
+//         <div class="card-body">
+//         <h5 class="card-title">${element.nombre}</h5>
+//         <p class="card-text">
+//         ${element.descripcion_corta}
+//         </p>
+//         <div class="card-text d-flex flex-row">
+//         <div><p>Colores:</p></div>
+//         <div class="d-flex colors">`;
 
-    element.colores.forEach((elementColor) => {
-      mensajeColor =
-        mensajeColor +
-        `<div class="producto-color-chico">
-                <img src="./images/productos/colores/${element.html_name}_${elementColor.url}.png" />
-                </div>`;
-    });
-    mensajeProductos = mensajeProductos + mensajeColor;
-    mensajeProductos =
-      mensajeProductos +
-      `</div>
-          </div>
+//   element.colores.forEach((elementColor) => {
+//     mensajeColor =
+//       mensajeColor +
+//       `<div class="producto-color-chico">
+//                 <img src="./images/productos/colores/${element.html_name}_${elementColor.url}.png" />
+//                 </div>`;
+//   });
+//   mensajeProductos = mensajeProductos + mensajeColor;
+//   mensajeProductos =
+//     mensajeProductos +
+//     `</div>
+//           </div>
 
-          <div class="card-product-detail-button align-self-end">
-          <a
-          href="./pages/productos/${element.html_name}.html"
-          class="btnMasDetalles"
-          >Más información</a
-          >
-          <a
-          href="./pages/productos/${element.html_name}.html"
-          class="btnCotizarProducto"
-                      >Cotizar</a
-                    >
-                  </div>
-                </div>
-              </div>
-            </div>`;
-  });
-  mensajeProductos = mensajeProductos + `</div>`;
-}
+//           <div class="card-product-detail-button align-self-end">
+//           <a
+//           href="./pages/productos/${element.html_name}.html"
+//           class="btnMasDetalles"
+//           >Más información</a
+//           >
+//           <a
+//           href="./pages/productos/${element.html_name}.html"
+//           class="btnCotizarProducto"
+//                       >Cotizar</a
+//                     >
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>`;
+// });
+mensajeProductos = mensajeProductos + `</div>`;
 
 document.getElementById("productosX").innerHTML = mensajeProductos;
 
